31e56adb2600dce8197aff2bbe061b9f8fd7298c
credbuzz <cred.buzz.ai@gmail.com>
2025-03-15 15:10:42 +0530

    refactor: modified the script for discarding campaign

--- oracle-mock/app.py
+++ oracle-mock/app.py
@@ -1,10 +1,9 @@
 from web3 import Web3
-from eth_typing import HexStr
-from eth_utils import to_bytes
 import json
 import os
 from dotenv import load_dotenv
 import time
+import requests
 
 # Load environment variables
 load_dotenv()
@@ -39,75 +38,83 @@ usdc_contract = w3.eth.contract(address=usdc_address, abi=usdc_abi)
 # Create account from private key
 owner = w3.eth.account.from_key(private_key)
 
-def get_campaign_info(campaign_id: bytes):
+def get_campaign_info(campaign_id: int):
     try:
-        return marketplace_contract.functions.getCampaignInfo(campaign_id).call()
-    except Exception as e:
-        raise Exception(f"Transaction failed: {str(e)}")
-        
-def accept_campaign(campaign_id: bytes):
-    try:
-        current_nonce = w3.eth.get_transaction_count(owner.address)
-        accept_txn = marketplace_contract.functions.acceptProjectCampaign(campaign_id).build_transaction({
-            'from': owner.address, 'nonce': current_nonce, 'gas': 100000, 'gasPrice': w3.eth.gas_price
-        })
-        sign_and_send_txn(accept_txn)
-        print(f"Accepted campaign {campaign_id.hex()}") 
+        campaign_info = marketplace_contract.functions.getCampaignInfo(campaign_id).call()
+        print(f"Campaign info: {campaign_info}")
+        return campaign_info
     except Exception as e:
         raise Exception(f"Transaction failed: {str(e)}")
 
-def handle_campaign(campaign_id: bytes):
+def discard_campaign(campaign_id: int):
     try:
+        url = f"{os.getenv('BASE_URL')}/update-campaign"
+        headers = {
+            "Content-Type": "application/json",
+            "x-api-key": os.getenv('TEST_API_KEY'),
+            "source": os.getenv('SOURCE')
+        }
+        body = {
+            "campaign_id": campaign_id,
+            "status": 'discarded'
+        }
+        response = requests.post(url, json=body, headers=headers)
+        return response.json()
+    except Exception as e:
+        raise Exception(f"Error discarding campaign {campaign_id}: {str(e)}")
+
+def handle_campaign(campaign_id: int):
+    print(f"Processing campaign {campaign_id}")
+    
+    try:
+        # get campaign info
         campaign_info = get_campaign_info(campaign_id)
-        creator = campaign_info[2]
-        kol = campaign_info[3]
-        full_amount = campaign_info[6] * 10 ** 6
-        amount = int(full_amount * 0.9) * 10 ** 6
+        
+        if campaign_info[1] == campaign_id:
+            print(f"INVALID CAMPAIGN ID: {campaign_id}")
+            return
+
+        # extract data
         status = campaign_info[7]
         current_nonce = w3.eth.get_transaction_count(owner.address)
 
-        if status == 1:  # ACCEPTED → Fulfill & Transfer Funds from Owner to Kol
-            fulfill_txn = marketplace_contract.functions.fulfilProjectCampaign(campaign_id).build_transaction({
-                'from': owner.address, 'nonce': current_nonce, 'gas': 200000, 'gasPrice': w3.eth.gas_price
-            })
-            sign_and_send_txn(fulfill_txn)
-            
-            transfer_txn = usdc_contract.functions.transfer(kol, amount).build_transaction({
-                'from': owner.address, 'nonce': current_nonce + 1, 'gas': 100000, 'gasPrice': w3.eth.gas_price
-            })
-            sign_and_send_txn(transfer_txn)
-            print(f"Transferred {amount} USDC to {kol}")
-        
-        elif status == 0 and w3.eth.get_block('latest').timestamp > campaign_info[4]:  # OPEN → Expired → Discard & Transfer Funds from Owner to Creator
-            discard_txn = marketplace_contract.functions.discardCampaign(campaign_id).build_transaction({
-                'from': owner.address, 'nonce': current_nonce, 'gas': 100000, 'gasPrice': w3.eth.gas_price
-            })
-            sign_and_send_txn(discard_txn)
 
-            transfer_txn = usdc_contract.functions.transfer(creator, full_amount).build_transaction({
-                'from': owner.address, 'nonce': current_nonce + 1, 'gas': 100000, 'gasPrice': w3.eth.gas_price
-            })
-            sign_and_send_txn(transfer_txn)
-            print(f"Transferred {full_amount} USDC to {creator}")
+        if status == 0:  # OPEN 
+            # check for the offer time and curretn time
+            offer_time = int(campaign_info[4] / 1000)
+            current_time = w3.eth.get_block('latest').timestamp
             
-            print(f"Discarded expired campaign {campaign_id.hex()}")
-        
-        elif status == 1 and w3.eth.get_block('latest').timestamp > campaign_info[5]:  # ACCEPTED → Expired → Unfulfill & Transfer Funds from Owner to Creator
-            unfulfill_txn = marketplace_contract.functions.unfulfillCampaign(campaign_id).build_transaction({
-                'from': owner.address, 'nonce': current_nonce, 'gas': 100000, 'gasPrice': w3.eth.gas_price
-            })
-            sign_and_send_txn(unfulfill_txn)
-            print(f"Marked campaign {campaign_id.hex()} as unfulfilled")
-
-            transfer_txn = usdc_contract.functions.transfer(creator, full_amount).build_transaction({
-                'from': owner.address, 'nonce': current_nonce + 1, 'gas': 100000, 'gasPrice': w3.eth.gas_price
-            })
-            sign_and_send_txn(transfer_txn)
-            print(f"Transferred {full_amount} USDC to {creator}")
+            if current_time > offer_time:
+                # discard the campaign
+                discard_txn = marketplace_contract.functions.discardCampaign(campaign_id).build_transaction({
+                    'from': owner.address, 'nonce': current_nonce, 'gas': 100000, 'gasPrice': w3.eth.gas_price
+                })
+                sign_and_send_txn(discard_txn)
+                print(f"Discarded campaign as offer time ended {campaign_id}")
+                discard_campaign(campaign_id)
+            else:
+                print(f"Offer time not ended for campaign {campaign_id}")
+                
+        elif status == 1:  # ACCEPTED 
+            promotion_end_time = campaign_info[5]
+            current_time = w3.eth.get_block('latest').timestamp
+            if current_time > promotion_end_time:
+                # fulfill the campaign
+                discard_txn = marketplace_contract.functions.discardCampaign(campaign_id).build_transaction({
+                    'from': owner.address, 'nonce': current_nonce, 'gas': 100000, 'gasPrice': w3.eth.gas_price
+                })
+                sign_and_send_txn(discard_txn)
+                print(f"Discarded campaign as promotion ended {campaign_id}")
+                discard_campaign(campaign_id)
+            else:
+                print(f"Promotion not ended for campaign {campaign_id}")
 
     except Exception as e:
-        raise Exception(f"Error handling campaign {campaign_id.hex()}: {str(e)}")
-        
+        print(f"Error handling campaign {campaign_id}: {str(e)}")
+        print(f"Campaign info: {campaign_info}")
+        print("--------------------------------\n\n")
+        # raise Exception(f"Error handling campaign {campaign_id}: {str(e)}")
+
 def sign_and_send_txn(txn: dict):
     signed_txn = w3.eth.account.sign_transaction(txn, private_key)
     tx_hash = w3.eth.send_raw_transaction(signed_txn.raw_transaction)
@@ -116,15 +123,22 @@ def sign_and_send_txn(txn: dict):
 
 def process_campaigns():
     try:
-        all_campaigns = marketplace_contract.functions.getAllCampaigns().call()
+        url = f"{os.getenv('BASE_URL')}/get-all-campaigns"
+        headers = {
+            "Content-Type": "application/json",
+            "x-api-key": os.getenv('TEST_API_KEY'),
+            "source": os.getenv('SOURCE')
+        }
+        response = requests.get(url, headers=headers)
+        all_campaigns = response.json().get('result')
+
         for campaign_id in all_campaigns:
             handle_campaign(campaign_id)
+
     except Exception as e:
         raise Exception(f"Error processing campaigns: {str(e)}")
 
 if __name__ == "__main__":
-    # all_campaigns = marketplace_contract.functions.getAllCampaigns().call()
-    # accept_campaign(all_campaigns[0])
 
     while True:
         print("\n\nProcessing campaigns...")
